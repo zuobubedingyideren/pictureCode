ca3626fcd40ef61c608d4e00dd2f8257e2687eab
px <3098263664@qq.com>
2025-12-21 19:03:51 +0800

    移除mock

--- picture-front/vite.config.ts
+++ picture-front/vite.config.ts
@@ -3,23 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import{ viteMockServe} from 'vite-plugin-mock'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     '/api': 'http://localhost:8123'
-  //   }
-  // },
-  // plugins: [vue(), vueDevTools()],
-  // resolve: {
-  //   alias: {
-  //     '@': fileURLToPath(new URL('./src', import.meta.url)),
-  //   },
-  // },
-  plugins:[vue(),viteMockServe({
-    mockPath:'mock',
-    enable:true
-  })]
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8123'
+    }
+  },
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
