4fa69f2f465c80d133c9502a1108ffe80fd0e751
px <3098263664@qq.com>
2025-12-26 11:35:30 +0800

    feat(api): 添加图片管理相关API接口
    - 新增fileController模块，包含文件上传下载功能
    - 新增tupianjiekou模块，提供图片管理完整功能
    - 添加图片类型定义，包括Picture、PictureVO、PictureQueryRequest等
    - 集成腾讯云COS配置和阿里云AI配置
    - 实现图片上传、删除、编辑、查询等API接口
    - 添加图片标签分类获取功能
    - 配置图片分页列表查询接口

--- picture-front/src/api/index.ts
+++ picture-front/src/api/index.ts
@@ -2,9 +2,13 @@
 /* eslint-disable */
 // API 更新时间：
 // API 唯一标识：
+import * as fileController from './fileController'
 import * as mainController from './mainController'
+import * as tupianjiekou from './tupianjiekou'
 import * as userController from './userController'
 export default {
+  fileController,
   mainController,
+  tupianjiekou,
   userController,
 }
---
+++ picture-front/src/api/tupianjiekou.ts
@@ -0,0 +1,157 @@
+// @ts-ignore
+/* eslint-disable */
+import request from '@/request'
+
+/** 删除图片 POST /api/picture/delete */
+export async function deletePictureUsingPost(
+  body: API.DeleteRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseBoolean_>('/api/picture/delete', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 编辑图片, 给用户使用 POST /api/picture/edit */
+export async function editPictureUsingPost(
+  body: API.PictureEditRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseBoolean_>('/api/picture/edit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 根据 id 获取图片, 仅管理员 GET /api/picture/get */
+export async function getPictureByIdUsingGet(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getPictureByIdUsingGETParams,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePicture_>('/api/picture/get', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  })
+}
+
+/** 根据 id 获取图片（封装类） GET /api/picture/get/vo */
+export async function getPictureVoByIdUsingGet(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getPictureVOByIdUsingGETParams,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePictureVO_>('/api/picture/get/vo', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  })
+}
+
+/** 分页获取图片列表, 仅管理员 POST /api/picture/list/page */
+export async function listPictureByPageUsingPost(
+  body: API.PictureQueryRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePagePicture_>('/api/picture/list/page', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 分页获取图片列表（封装类） POST /api/picture/list/page/vo */
+export async function listPictureVoByPageUsingPost(
+  body: API.PictureQueryRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePagePictureVO_>('/api/picture/list/page/vo', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 获取标签和分类 GET /api/picture/tag_category */
+export async function listPictureTagCategoryUsingGet(options?: { [key: string]: any }) {
+  return request<API.BaseResponsePictureTagCategory_>('/api/picture/tag_category', {
+    method: 'GET',
+    ...(options || {}),
+  })
+}
+
+/** 更新图片, 仅管理员 POST /api/picture/update */
+export async function updatePictureUsingPost(
+  body: API.PictureUpdateRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseBoolean_>('/api/picture/update', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 上传图片 POST /api/picture/upload */
+export async function uploadPictureUsingPost(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.uploadPictureUsingPOSTParams,
+  body: {},
+  file?: File,
+  options?: { [key: string]: any }
+) {
+  const formData = new FormData()
+
+  if (file) {
+    formData.append('file', file)
+  }
+
+  Object.keys(body).forEach((ele) => {
+    const item = (body as any)[ele]
+
+    if (item !== undefined && item !== null) {
+      if (typeof item === 'object' && !(item instanceof File)) {
+        if (item instanceof Array) {
+          item.forEach((f) => formData.append(ele, f || ''))
+        } else {
+          formData.append(ele, new Blob([JSON.stringify(item)], { type: 'application/json' }))
+        }
+      } else {
+        formData.append(ele, item)
+      }
+    }
+  })
+
+  return request<API.BaseResponsePictureVO_>('/api/picture/upload', {
+    method: 'POST',
+    params: {
+      ...params,
+    },
+    data: formData,
+    requestType: 'form',
+    ...(options || {}),
+  })
+}
